f394523c1f751e5a7710a39c03ba6bb8f3bee831
이형준 <49009864+kokocan12@users.noreply.github.com>
2022-08-22 22:14:56 +0900

    [Feature] ImgBox Component 구현완료 (#73)

---
+++ client/src/components/ImgBox.tsx
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+import { colors } from './Color';
+
+const ImgBoxCommon = styled.div`
+  border: 1px solid ${colors.gray3};
+  background-color: ${colors.offWhite};
+  overflow: hidden;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const SmallImgBox = styled(ImgBoxCommon)`
+  width: 40px;
+  height: 40px;
+  border-radius: 6px;
+`;
+
+const MediumImgBox = styled(ImgBoxCommon)`
+  width: 76px;
+  height: 76px;
+  border-radius: 8px;
+`;
+
+const LargeImgBox = styled(ImgBoxCommon)`
+  width: 106px;
+  height: 106px;
+  border-radius: 10px;
+`;
+
+const Img = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+
+const Gradient = styled.div<{
+  width?: number | string;
+  height?: number | string;
+}>`
+  background: linear-gradient(
+    180deg,
+    rgba(0, 0, 0, 0.24) 0%,
+    rgba(0, 0, 0, 0) 16.52%,
+    rgba(0, 0, 0, 0) 87.36%,
+    rgba(0, 0, 0, 0.24) 100%
+  );
+
+  ${(props) => (props.width ? `width: ${props.width}px;` : '')}
+  ${(props) => (props.height ? `height: ${props.height}px;` : '')}
+`;
+
+export const ImgBox = (props: React.ImgHTMLAttributes<HTMLImageElement>) => {
+  return <SmallImgBox>{props.src && <Img {...props} />}</SmallImgBox>;
+};
+
+ImgBox.Small = ImgBox;
+
+ImgBox.Medium = (props: React.ImgHTMLAttributes<HTMLImageElement>) => {
+  return <MediumImgBox>{props.src && <Img {...props} />}</MediumImgBox>;
+};
+
+ImgBox.Large = (props: React.ImgHTMLAttributes<HTMLImageElement>) => {
+  return <LargeImgBox>{props.src && <Img {...props} />}</LargeImgBox>;
+};
+
+ImgBox.Gradient = (props: { width: number; height: number }) => {
+  return <Gradient {...props} />;
+};
